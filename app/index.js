aaf3107ff69b176829288211d3241ad103d26538
James Howard <james@plumpdigital.co.uk>
2014-10-23 12:17:53 +0100

    Initial commit.

---
+++ app/index.js
@@ -0,0 +1,130 @@
+'use strict';
+
+var util      = require('util'),
+	path      = require('path'),
+	generator = require('yeoman-generator'),
+	chalk     = require('chalk');
+
+/**
+ *    Plump SilverStripe Composer modules.
+ *
+ *    Prepended with plumpss- before being added as dependencies to composer.json
+ */
+var plumpSilversStripeModules = [
+
+];
+
+/**
+ *    Bower Inuit package options (prepended with inuitcss-).
+ *    Organise alphabetically within Shearing layer category.
+ */
+var inuitModules = [
+
+	// Settings
+	'defaults',
+	'responsive-settings',
+
+	// Tools
+	'functions',
+	'mixins',
+	'responsive-tools',
+
+	// Generic
+	'box-sizing',
+	'normalize',
+	'reset',
+	'shared',
+
+	// Base
+	'headings',
+	'images',
+	'lists',
+	'page',
+	'paragraphs',
+
+	// Object
+	'box',
+	'buttons',
+	'flag',
+	'layout',
+	'list-bare',
+	'list-block',
+	'list-inline',
+	'list-ui',
+	'media',
+	'pack',
+	'tables',
+	'tabs',
+
+	// Trumps
+	'clearfix',
+	'print',
+	'spacing',
+	'spacing-responsive',
+	'widths',
+	'widths-responsive'
+];
+
+/**
+ *    Bower Plump package options (prepended with plumpcss-).
+ *    Organise alphabetically within shearing layer category.
+ */
+var plumpModules = [
+
+	// Settings
+	'defaults',
+
+	// Tools
+	'mixins',
+
+	// Objects
+	'band',
+	'band-responsive',
+	'exhibit',
+	'meter',
+	'nav-list',
+	'stack',
+	'wrapper',
+
+	// Trumps
+	'floats',
+	'hide',
+	'text-align'
+];
+
+/**
+ *    Yeoman generator class. Each method (not starting with _) is
+ *    executed in source order.
+ */
+module.exports = generator.Base.extend({
+
+	init: function () {
+		this.pkg = require('../package.json');
+
+		this.on('end', function () {
+			this.log(chalk.magenta.bold('I\'m all done. You now need to run ') +
+					 chalk.green.bold('npm install ') +
+					 chalk.magenta.bold('& ') +
+					 chalk.green.bold('bower install'));
+		});
+	},
+
+	/**
+	 *    Prompts for project configuration.
+	 *
+	 * 1. Create asynchronous callback to pause execution
+	 *    until all the prompts are completed.
+	 */
+	askFor: function () {
+		var done = this.async(); // [1]
+
+		// Have Yeoman greet the user.
+		this.log(this.yeoman);
+
+		// replace it with a short and sweet description of your generator
+		this.log(chalk.magenta('You\'re using the Plump SilverStripe generator v' + this.pkg.version));
+
+
+	}
+
+});
