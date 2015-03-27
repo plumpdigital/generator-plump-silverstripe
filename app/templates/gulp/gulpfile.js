871a9bb511fef22c63d66e1e09048b4aa1cc3523
James Martin-Davies <jamesmd@plumpdigital.co.uk>
2015-03-27 14:36:39 +0000

    Refactor build process. Change required modules. Add error handler. Refactor LiveReload process.

--- app/templates/gulp/gulpfile.js
+++ app/templates/gulp/gulpfile.js
@@ -1,7 +1,6 @@
 //    Gulp utility.
 var gulp    = require('gulp'),
-	gutil   = require('gulp-util'),
-	plumber = require('gulp-plumber');
+	gutil   = require('gulp-util');
 
 //    Requires.
 var express     = require('express'),
@@ -17,11 +16,12 @@ var concat       = require('gulp-concat'),
 	jshint       = require('gulp-jshint'),
 	rename       = require('gulp-rename'),
 	swig         = require('gulp-swig'),
-	sass         = require('gulp-ruby-sass'),
+	sass         = require('gulp-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
 	minifycss    = require('gulp-minify-css'),
 	imagemin     = require('gulp-imagemin'),
 	newer        = require('gulp-newer'),
+	insert       = require('gulp-insert'),
 	livereload   = require('gulp-livereload'),
 	ftp          = require('gulp-ftp');
 
@@ -32,10 +32,26 @@ var config = require('./gulp-config.json'),
 //    Load command-line arguments.
 var argv = require('yargs').argv;
 
-//    Error handler function.
-var onError = function (err) {
-	console.log(err);
-};
+/**
+ *    Make error handling easier by outputting the error message
+ *    directly in to the browser.
+ *
+ * 1. Use the already-built CSS file in the dev directory.
+ * 2. Prepend content in to the CSS file. Use regex to remove
+ *    and new lines & escape single quotes which would break the CSS.
+ * 3. Write the file to the dev/css directory.
+ */
+var injectError = function(err) {
+
+	// Log error as normal.
+	console.log(err.message + ' on Line ' + err.line + ' , Column ' + err.column + '.');
+
+	gulp.src('dev/css/style.css') /* [1] */
+		.pipe(insert.prepend("body:before { content: '" + /* [2] */
+			'Sass Build Error: ' + err.message.replace(/(\r\n|\n|\r)/gm," ").replace(/'/g, "\\'") +
+			' on Line ' + err.line + ', Column ' + err.column + '.' + "'; color: red; font-weight: bold }"))
+		.pipe(gulp.dest('dev/css')) /* [3] */
+}
 
 /**
  *    Constants.
@@ -73,35 +89,36 @@ gulp.task('scripts', function() {
 
 });
 
-/**
- *    Styles build task. Compiles CSS from SASS, auto-prefixes
- *    and outputs both a minified and non-minified version into
- *    dist/ and dev/ respectively.
- *
- * 1. Using all files defined in files.styles config.
- * 2. Catch any errors within the SASS build pipeline.
- * 3. Compile using SASS, expanded style.
- * 4. Auto-prefix (e.g. -moz-) using last 2 browser versions.
- * 5. Output prefixed but non-minifed CSS.
- * 6. Rename to .min.css
- * 7. Minify the CSS.
- * 8. Output prefixed, minified CSS.
- */
+ /**
+  *    Styles build task. Compiles CSS from SASS, auto-prefixes
+  *    and outputs both a minified and non-minified version into
+  *    dist/ and dev/ respectively.
+  *
+  * 1. Using all files defined in files.styles config.
+  * 2. Compile using SASS, expanded style.
+  * 3. Auto-prefix (e.g. -moz-) using last 2 browser versions.
+  * 4. Output prefixed but non-minifed CSS to dev/css
+  * 5. Rename to .min.css
+  * 6. Minify the CSS.
+  * 7. Output prefixed, minified CSS to dist/css.
+  */
+ gulp.task('styles', function() {
 
-gulp.task('styles', function() {
+ 	var outputDirectory = themeDirectory + 'css/';
 
-	var outputDirectory = themeDirectory + 'css/';
+ 	return gulp.src(config.files.styles) /* [1] */
+ 		.pipe(sass({  /* [2] */
+ 			style : 'expanded',
+ 			onError: injectError
+ 		}))
+ 		.pipe(autoprefixer('last 2 versions')) /* [3] */
+ 		.pipe(gulp.dest(outputDirectory))  /* [4] */
+ 		.pipe(rename({ suffix : '.min' })) /* [5] */
+ 		.pipe(minifycss()) /* [6] */
+ 		.pipe(gulp.dest(outputDirectory)); /* [7] */
 
-	return gulp.src(config.files.styles) /* [1] */
-		.pipe(plumber(onError)) /* [2] */
-		.pipe(sass({ style : 'expanded' })) /* [3] */
-		.pipe(autoprefixer('last 2 versions')) /* [4] */
-		.pipe(gulp.dest(outputDirectory)) /* [5] */
-		.pipe(rename({ suffix : '.min' })) /* [6] */
-		.pipe(minifycss()) /* [7] */
-		.pipe(gulp.dest(outputDirectory)); /* [8] */
+ });
 
-});
 
 /**
  *    Image optimsation task.
@@ -187,9 +204,7 @@ gulp.task('develop', ['build', 'watch'] /* [1] */, function() {
 
 	livereload.listen(LIVERELOAD_PORT); /* [2] */
 
-	gulp.watch(themeDirectory + '/**/*').on('change', function(file) { /* [3] */
-		livereload.changed(file.path);
-	});
+	gulp.watch(themeDirectory + '/**/*').on('change', livereload.changed);
 
 });
 
